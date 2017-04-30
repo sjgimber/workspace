a6393343ba09852a3b67c62d5dab7d05693c03c9
John Gimber <john.gimber@qualityscape.co.uk>
2017-04-30 11:38:13 +0100

    Try

--- scotland-demo/src/HelperFunctions/constants.java
+++ scotland-demo/src/HelperFunctions/constants.java
@@ -11,11 +11,14 @@ public class constants
 	// NOTE: In future, it should be possible to set this via command-line, to allow for flexible calling from CI systems.
 	public static final String baseURL = "http://scotland-demo.trapezegroup.co.uk";
 
-	// Screen titles.  Used for navigation checking.
-	public static final String titleHomePage = "Home | Traveline Scotland";
-	public static final String titleJourneyPlannerPage = "Traveline Scotland - Public transport information";
+	// Screen URLs.  Used for navigation checking.
+	public static final String urlHomePage 		= "/?lang=en";
+	public static final String titleHomePage 	= "Home | Traveline Scotland";
 	
-	
+	public static final String urlJourneyPlannerPage = "/travelInfo";
+	public static final String urlDeparturesPage = "/liveDepartures";
+	public static final String urlTimetablesPage = "/timetables";
+	public static final String urlTravelUpdatesPage = "/updates";
 	
 	// List of invalid date values to use for validation checking of each date field.
     public static final String[] invalidDates =	{"2017", "2017-02-29", "invalidvalue"};
